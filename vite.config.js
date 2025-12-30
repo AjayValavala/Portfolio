a1305cc08ac7f959ecd1b81cced55b4745b169b3
AjayValavala <valavalasatyasaiajay@gmail.com>
2025-12-30 18:22:17 +0530

    Update Vite config to enable React plugin
    Uncommented and updated Vite configuration to include React plugin.

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-export default {
+
+/*export default {
   base: '/Portfolio/',
-}
+}*/
 
 // https://vitejs.dev/config/
-/*export default defineConfig({
-  //plugins: [react()]
-})*/
+export default defineConfig({
+  plugins: [react()]
+})
 
